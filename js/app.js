5ff75f3ecd6de73130653ea3a4c82506bddf99a8
FinnCavanagh <f.c.cavanagh@gmail.com>
2016-01-07 17:30:42 +0000

    added self calling function to determine API_URL! shabam

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 angular
   .module('power', ['angular-jwt', 'ngResource'])
-  .constant('API', 'http:///localhost:3000/api')
+  .constant('API', (function(){
+    var domain = /localhost/.test(document.domain) ? 'localhost:3000' : 'send-power.herokuapp.com'
+      return 'http://'+ domain + '/api'
+  })() )
   .config(function($httpProvider) {
     $httpProvider.interceptors.push('AuthInterceptor');
   });
